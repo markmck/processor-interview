771d016176e8943a2667fb166452624decadc8c2
Mark M <markmck02@gmail.com>
2025-07-10 20:17:25 -0500

    feat: add swagger ui for easier testing. small clean up/debuggin fixes

--- back-end/ProcessorAPI/ProcessorAPI/Program.cs
+++ back-end/ProcessorAPI/ProcessorAPI/Program.cs
@@ -9,15 +9,29 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ProcessorDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ProcessorDb")));
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Add OpenAPI/Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Processor API",
+        Version = "v1",
+        Description = "API for processing credit card transactions"
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Card Processor API v1");
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
